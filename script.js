016987c1b93cd1c5e951339912144ad703167ff5
FabulousErin <77863014+FabulousErin@users.noreply.github.com>
2021-06-18 19:05:09 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
+var dataForSinglePerson;
+
 d3.json("samples.json").then(function(data) {
     console.log(data);
   var single = data.samples[0]
-bubbleChart()
-
 
     // Select the input value from the form
     var selectTag = d3.select("#selDataset")
@@ -11,8 +11,7 @@ bubbleChart()
       selectTag.append('option').text(data.names[i]).property('value',data.names[i] )
         
     }
-  
-  
+   
 });
 
 function optionChanged(names) {
@@ -22,10 +21,10 @@ function optionChanged(names) {
         console.log('Loaded json file second time~!',data);
       var single = data.samples[0]
 
-    var dataForSinglePerson;
+   // var dataForSinglePerson;
 
     for (let i = 0; i< data.samples.length; i++) {
-        console.log('each sample in loo[p~!!!', data.samples[i])
+        console.log('each sample in loop~!!!', data.samples[i])
         if(data.samples[i]){
         console.log('this is i', i)
         if (names === data.samples[i].id){
@@ -37,62 +36,94 @@ function optionChanged(names) {
     }
     console.log('We found a match!!', dataForSinglePerson)
     
-    function buildTable(age, bbtype, ethnicity, gender, id, location) {
+    var dataForMeta
+    for (let i = 0; i< data.metadata.length; i++) {
+      console.log('each metadata info in loop~!!!', data.metadata[i])
+      if(data.metadata[i]){
+     // console.log('this is i', i)
+      if (names == data.metadata[i].id){
+        console.log('HIT META IFF!!!')
+          dataForMeta = data.metadata[i]
+
+      }
+  }
+  
+  }
+
+  console.log('mettaaaaa', dataForMeta)
+
+    function buildTable(data) {
+      console.log('dataaaa in build table', data)
         var table = d3.select("#sample-metadata");
+        table.html("")
         var tbody = table.select("panel-body");
         var trow;
-        for (var i = 0; i < 12; i++) {
+       // for (var i = 0; i < 12; i++) {
           trow = tbody.append("tr");
-          trow.append("td").text(age[i]);
-          trow.append("td").text(bbtype[i]);
-          trow.append("td").text(ethnicity[i]);
-          trow.append("td").text(gender[i]);
-          trow.append("td").text(id[i]);
-          trow.append("td").text(location[i]);
-        }
+          table.append("p").text("Age: " + data.age);
+          table.append("p").text("Bbtype: " + data.bbtype);
+          table.append("p").text("Ethnicity:" + data.ethnicity);
+          table.append("p").text("Gender:" + data.gender);
+          table.append("p").text("ID:" + data.id);
+          table.append("p").text("Location:" + data.location);
+       // }
       }
-    
+    buildTable(dataForMeta);
 
-  var trace1 = {
-      x: dataForSinglePerson.otu_ids,
-      y: dataForSinglePerson.sample_values,
-      type: "bar"
-    };
-    
+  var trace1 = [{
+      values: dataForSinglePerson.otu_ids,
+      labels: dataForSinglePerson.sample_values,
+      type: "pie"
+    }];
+
+  var layout = {
+    height: 400,
+    width: 500
+  };
     // Create the data array for the plot
     var data = [trace1];
     
     // Define the plot layout
-    var layout = {
-      title: "Top 10 OTUs",
-      xaxis: { title:  `Data for ${dataForSinglePerson.id}`},
-      yaxis: { title: "Value" }
-    };
+    // var layout = {
+    //   title: "Top 10 OTUs",
+    //   xaxis: { title:  `Data for ${dataForSinglePerson.id}`},
+    //   yaxis: { title: "Value" }
+    // };
     
     // Plot the chart to a div tag with id "bar"
-    Plotly.newPlot("bar", data, layout);
-    buildTable()
+    Plotly.newPlot("pie", trace1, layout);
+    
+    bubbleChart()
     
     });
 }
 
     function bubbleChart(names) {
         d3.json("samples.json").then(function(data) {
-           console.log("bubble data", data)
-           var samples = data.samples;
-           var resultArray = samples.filter(sampleObj => sampleObj.id == names);
-           var result = resultArray[0];
+          console.log('buble chart data', data)
+          console.log('We found a match Bubble!!', dataForSinglePerson)
+          
+          //  var samples = data.samples;
+          //  console.log("samples are", samples)
+          //  var resultArray = samples.filter(sampleObj => sampleObj.id == names);
+          //  var result = resultArray[0];
         
         // Create the data array for the plot
-      
-            
+                 
       var trace2 = {
-          x: result.otu_ids,
-          y: result.sample.sample_values,
-          type: "scatter"
+          x: dataForSinglePerson.otu_ids,
+          y: dataForSinglePerson.sample_values,
+          text: dataForSinglePerson.otu_labels,
+          //type: "box"
+          mode: 'markers',
+          marker: {
+            size:  dataForSinglePerson.sample_values,
+            color:  dataForSinglePerson.otu_ids,
+            colorscale: 'Earth'
+          }
         };
-        console.log('x axis values are', data.names)
-        console.log('y axis values are', data.samples.sample_values)
+        console.log('x axis values are', dataForSinglePerson.names)
+        console.log('y axis values are', dataForSinglePerson.sample_values)
 
         // Create the data array for the plot
         var graphdata = [trace2];
